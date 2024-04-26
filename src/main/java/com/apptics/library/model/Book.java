f3f347caf18ed9b7bcacbd942e90c94390f12bd6
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-26 06:09:52 -0500

    modelos y conexion a base de datos h2

--- src/main/java/com/apptics/library/model/Book.java
+++ src/main/java/com/apptics/library/model/Book.java
@@ -1,10 +1,16 @@
 package com.apptics.library.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Book {
 
--- src/main/java/com/apptics/library/model/Category.java
+++ src/main/java/com/apptics/library/model/Category.java
@@ -1,7 +1,13 @@
 package com.apptics.library.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Category {
 
