d2f185e686fc6cea9f40d427ea0f8ee82e5ae8a1
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-05-04 10:32:15 -0500

    guardar libros y redireccionar a la pagina que contiene todos los libros

--- src/main/java/com/apptics/library/controller/BookController.java
+++ src/main/java/com/apptics/library/controller/BookController.java
@@ -1,19 +1,31 @@
 package com.apptics.library.controller;
 
 import com.apptics.library.model.Book;
+import com.apptics.library.service.AuthorService;
 import com.apptics.library.service.BookService;
+import com.apptics.library.service.CategoryService;
+import com.apptics.library.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Controller
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private PublisherService publisherService;
+    @Autowired
+    private AuthorService authorService;
 
 
     @GetMapping("/books")
@@ -29,4 +41,23 @@ public class BookController {
         model.addAttribute("book", book);
         return "book";
     }
+
+    @GetMapping("/add-book")
+    public String addBook(Book book, Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("publishers", publisherService.getAllPublishers());
+        model.addAttribute("authors", authorService.getAllAuthors());
+        return "add-book";
+    }
+
+    @PostMapping("/save-book")
+    public String saveBook(Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-book";
+        }
+        bookService.saveBook(book);
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books";
+    }
+
 }
