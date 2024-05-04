6af278a1c92e4abd82e076cf93a2133e0e1593de
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-05-03 22:03:33 -0500

    ver el detalle del libro

--- src/main/java/com/apptics/library/controller/BookController.java
+++ src/main/java/com/apptics/library/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +21,12 @@ public class BookController {
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
         return "books";
+    }
+
+    @GetMapping("/book/{id}")
+    public String book(Model model, @PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        model.addAttribute("book", book);
+        return "book";
     }
 }
